d50ae89d2395e6a4d3d78da56cb2897069abf479
Diego <diego.santospo@gmail.com>
2017-11-21 21:13:19 -0200

    Configuracao dao.

--- src/main/java/br/edu/facear/dao/DocumentoDao.java
+++ src/main/java/br/edu/facear/dao/DocumentoDao.java
@@ -1,5 +1,38 @@
 package br.edu.facear.dao;
 
-public class DocumentoDao {
+import javax.persistence.EntityManager;
 
+import br.edu.facear.model.Documento;
+
+public class DocumentoDao extends DAO{
+
+	EntityManager em = getEntityManager();
+	
+	public void salvar(Documento documento){
+
+		try {
+
+			em.getTransaction().begin();
+
+			if (documento.getId() != null) {
+
+				//faz a edicao 
+				em.merge(documento);
+
+			} else {
+
+				//faz a persistencia
+				em.persist(documento);
+
+			}
+
+			//salva no BD
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			//se der erro, ele cancela o insert
+			em.getTransaction().rollback();
+		}
+
+	}
+	
 }
--- src/main/java/br/edu/facear/model/Documento.java
+++ src/main/java/br/edu/facear/model/Documento.java
@@ -2,7 +2,11 @@ package br.edu.facear.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -14,6 +18,17 @@ public class Documento implements Serializable {
 	 */
 	private static final long serialVersionUID = 6210257839290256101L;
 
-	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_ATUACAO", nullable = false)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 	
 }
